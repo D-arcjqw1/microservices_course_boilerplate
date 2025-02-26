2c3111826dd8d9161661e56074be112daa8c4a6b
DDD <117805126+D-arcjqw1@users.noreply.github.com>
2025-02-26 21:38:26 +0300

    1

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,5 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
-	fmt.Println(color.GreenString("Hello, world!1"))
-=======
 	fmt.Println(color.GreenString("Hello, world!2"))
->>>>>>> 30e75d90f8496f575730c0986d0b34079949caae
 }
